0f88759d87a7493613c7026c8f9c4dc75f168de8
Chris Soliday <solidaycl@gmail.com>
2018-03-15 18:57:50 -0400

    Started skeleton for integration testing server

--- Server/Server_test.go
+++ Server/Server_test.go
@@ -1,17 +1,71 @@
 package main
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+//	"fmt"
+	"net/http"
+	"time"
+)
 
-func TestSum5and5(t *testing.T) {
-	total := Sum(5, 5)
-	if total != 10 {
-		t.Errorf("Sum incorrect, got: %d, want: %d.", total, 10)
+
+const serverAddress = "solidaycl.com:8080/"
+
+
+
+var myClient = &http.Client{Timeout: 10 * time.Second}
+
+func getJson(url string, target interface{}) error {
+	r, err := myClient.Get(url)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+
+	return json.NewDecoder(r.Body).Decode(target)
+}
+
+
+
+type Foo struct {
+	Bar string
+}
+
+
+func TestGetParties(t *testing.T) {
+
+	foo1 := new(Foo) // or &Foo{}
+	getJson(serverAddress + "/party", foo1)
+	t.Errorf(foo1.Bar)
+
+	// parse the json string
+	/*
+	if err != nil {
+		t.Errorf("returned error: " + err.Error())
+	}
+	*/
+}
+
+//TODO test get a party
+func TestGetParty(t *testing.T) {
+	if true {
+		t.Errorf("error msg")	
 	}
 }
 
-func TestSum3and5(t *testing.T) {
-	total := Sum(3, 5)
-	if total != 8 {
-		t.Errorf("Sum incorrect, got: %d, want: %d.", total, 8)
+
+//TODO 
+func TestCreateParty(t *testing.T) {
+	if true {
+		t.Errorf("error msg")	
 	}
 }
+
+//TODO 
+func TestDeleteParty(t *testing.T) {
+	if true {
+		t.Errorf("error msg")	
+	}
+}
+
+//TODO create more tests
