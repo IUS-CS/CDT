39140a21af4b290c12b146cf33c7b7dd8739ef34
DBeganovic01 <dbeganovic7@gmail.com>
2018-04-19 15:24:46 -0400

    Added ImageView on JoinActivity that will have the thumbnail of the currrent song. Not implemented yet

--- App/app/src/main/java/cdt/app/HostActivity.java
+++ App/app/src/main/java/cdt/app/HostActivity.java
@@ -13,6 +13,7 @@ import android.view.View;
 
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
